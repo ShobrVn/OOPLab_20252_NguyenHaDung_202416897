6abef512df5aac19fd8c60e324fb0ca829b67122
ShobrVn <nguyenhadung3007@gmail.com>
2026-04-04 19:17:39 +0700

    First Commit

---
+++ Lab01/InputFromKeyboard.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class InputFromKeyboard {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.print("What's your name? ");
+        String strName = keyboard.nextLine();
+        System.out.print("How old are you? ");
+        int iAge = keyboard.nextInt();
+        System.out.print("How tall are you? ");
+        double dHeight = keyboard.nextDouble(); 
+
+        System.out.println("Mrs/Mr. " + strName + ", " + iAge + " years old. Your Height is "
+            + dHeight + ".");
+    }
+}
