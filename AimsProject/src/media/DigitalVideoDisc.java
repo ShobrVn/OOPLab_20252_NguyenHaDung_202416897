75cb4338a1d0c6cbffbf45195ac2a45fe8d18788
ShobrVn <nguyenhadung3007@gmail.com>
2026-06-10 15:18:31 +0700

    add feature throw exception

--- AimsProject/src/media/DigitalVideoDisc.java
+++ AimsProject/src/media/DigitalVideoDisc.java
@@ -1,5 +1,7 @@
 package src.media;
 
+import src.exception.PlayerException;
+
 public class DigitalVideoDisc extends Disc implements Playable {
     private String director;
     private int length;
@@ -37,7 +39,11 @@ public class DigitalVideoDisc extends Disc implements Playable {
     }
 
     @Override
-    public void play() {
+    public void play() throws PlayerException {
+        if (this.getLength() <= 0) {
+            throw new PlayerException("ERROR: DVD length is non-positive!");
+        }
+        
         System.out.println("Playing DVD: " + this.getTitle());
         System.out.println("DVD length: " + this.getLength());
     }
